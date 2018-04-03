4690b9570d3bef505a384e2d0f9bf1613a06ad87
wangwei <wei.wang40@hpe.com>
2018-04-03 11:05:24 +0800

    del

--- msbs_report - Copy.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-taskkill /f /im javaw.exe
-ping 127.0.0.1 -n 3
-start javaw -jar C:\msi_dev\msbireport-0.0.1-SNAPSHOT.war
-exit
--- msbs_report_java.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-taskkill /f /im java.exe
-ping 127.0.0.1 -n 3
-java -jar C:\msi_dev\msbireport-0.0.1-SNAPSHOT.war
